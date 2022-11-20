5b613fdadbb7ad50d36dae9f2a76c29c681062f1
mesik <71527914+mesiik@users.noreply.github.com>
2022-11-20 16:00:50 +0100

    Delete schema.sql

--- specty/database/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS prefixes(
-    guild_id BIGINT PRIMARY KEY NOT NULL, 
-    prefix TEXT DEFAULT (';')
-    );
-CREATE TABLE IF NOT EXISTS lastfm(
-    user_id BIGINT PRIMARY KEY NOT NULL, 
-    username TEXT
-);
